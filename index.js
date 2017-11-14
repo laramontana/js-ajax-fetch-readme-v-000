90a82a0a85ce938ad34aa4d352413483ec1ad8a1
laramontana <larabrightside@gmail.com>
2017-11-15 00:11:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+// fetch('https://api.github.com/repos/jquery/jquery/commits')
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+
+const token = "314343e5b147165421a89ad60b45c305c7fee82a";
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
